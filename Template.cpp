4067052bf58bee512e745c519b9554c44db64e72
Roberticey <48215918+Roberticey@users.noreply.github.com>
2020-04-13 23:44:36 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <map>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+#include <string.h>
+ 
+#define rep(i,n)    for(ll i=0;i<n;i++)
+#define repu(i,k,n) for(ll i=k;i<=n;i++)
+#define repd(i,k,n) for(ll i=k;i>=n;i--)
+#define se second
+#define fi first
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+#define INF (ll)ll_MAX
+ 
+typedef long long ll;
+typedef long double ld;
+typedef std::pair<ll, ll> pii;
+typedef std::vector<ll> vi;
+typedef std::vector< pii > vii;
+typedef std::vector< ll > vll;
+typedef std::vector< std::pair < pii, ll > > vpp;
+ 
+const ll N = 3*1e5 +5 ;
+const long long MOD = 1000000007;
+const long long MOD2 = 998244353;
+const long long inf = 1e18;
+ 
+using namespace std;
+ 
+void solve()
+{	
+    
+}
+     
+int main() {
+    #ifndef ONLINE_JUDGE
+        // for getting input from input.txt
+        freopen("input.txt", "r", stdin);
+        // for writing output to output.txt
+        freopen("output.txt", "w", stdout);
+    #endif
+    //cout<<"helo";
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    ll t = 1;
+  //  cin>>t;
+    
+    while(t--)
+    {
+        solve();    
+    } 
+}
